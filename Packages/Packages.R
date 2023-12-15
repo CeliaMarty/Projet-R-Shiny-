25aed2cab8e462f6c2f1d4397c2d242c40165572
Célia MARTY <celiamartypro@gmail.com>
2023-12-15 14:16:45 +0100

    gitignore

---
+++ Packages/Packages.R
@@ -0,0 +1,4 @@
+library(shiny)
+library(tidyverse)
+library(leaflet.providers)
+library(shinydashboard)
---
+++ app/app.R
@@ -0,0 +1,51 @@
+library(shiny)
+    ui <- fluidPage(
+      titlePanel("L.A DATA CRIME"),
+            br(),
+            br(),
+            br(),
+            navlistPanel(
+            "MENU",
+            br(),
+            br(),
+            br(),
+            tabPanel("HOME",
+                     ),
+            br(),
+            br(),
+            br(),
+            tabPanel("VICTIMS", 
+            ),
+            br(),
+            br(),
+            br(),
+            tabPanel("LOCATION",
+            ),
+            br(),
+            br(),
+            br(),
+            tabPanel("TYPE OF CRIME",
+            ),
+        mainPanel(
+          img(src = "losangeles.jpeg", alt = "Image"),
+              h3("SOME NUMBERS"),
+              valueBoxOutput("maBox"),
+              valueBoxOutput("maBox1")
+          
+        )
+        )
+            )
+server <- function(input, output)
+{
+  output$maBox <- renderValueBox({
+    valueBox(123, "Nombre de délits",
+             icon = icon("exclamation-triangle"))
+  })
+  output$maBox1 <- renderValueBox({
+    valueBox(122, "Nombre d'habitants",
+             icon = icon("users"))
+    })
+}    
+shinyApp(ui = ui, server = server)
+
+
